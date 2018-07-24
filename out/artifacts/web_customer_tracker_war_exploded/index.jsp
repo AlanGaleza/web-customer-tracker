7a63e0f14bb623a46d2b214c0072ec270d856860
“AlanGaleza <alano.gal@gmail.com>
2018-07-24 18:32:07 +0200

    list-customers.jsp

--- out/artifacts/web_customer_tracker_war_exploded/index.jsp
+++ out/artifacts/web_customer_tracker_war_exploded/index.jsp
@@ -8,9 +8,9 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
   <head>
-    <title>$Title$</title>
+    <title>Menu</title>
   </head>
   <body>
-  $END$
+  <a href="/customer/list">List Customer</a>
   </body>
 </html>
--- src/com/luv2code/springdemo/controller/CustomerController.java
+++ src/com/luv2code/springdemo/controller/CustomerController.java
@@ -1,15 +1,30 @@
 package com.luv2code.springdemo.controller;
 
+import com.luv2code.springdemo.dao.CustomerDAO;
+import com.luv2code.springdemo.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+    @Autowired
+    private CustomerDAO customerDAO;
+
     @RequestMapping("/list")
     public String listCustomers(Model theModel) {
+
+        //get customers from the dao
+        List<Customer> theCustomers = customerDAO.getCustomers();
+
+        //add the customers to the model
+        theModel.addAttribute("customers", theCustomers);
+
         return "list-customers";
     }
 }
