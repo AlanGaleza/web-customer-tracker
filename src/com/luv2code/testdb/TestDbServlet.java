0140c3129b4dd4395a02b202a73bb820aa97d6db
“AlanGaleza <alano.gal@gmail.com>
2018-07-24 17:15:24 +0200

    first commit

---
+++ src/com/luv2code/testdb/TestDbServlet.java
@@ -0,0 +1,46 @@
+package com.luv2code.testdb;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import java.sql.*;
+
+@WebServlet(name = "TestDbServlet")
+public class TestDbServlet extends HttpServlet {
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //setup connection variables
+        String user = "springstudent";
+        String pass = "springstudent";
+
+        String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?serverTimezone=Europe/Warsaw";
+
+        String driver = "com.mysql.jdbc.Driver";
+
+        //get connection to database
+        try {
+            PrintWriter out = response.getWriter();
+
+            out.println("connecting to databse: " + jdbcUrl);
+
+            Class.forName(driver);
+
+            Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
+
+            out.println("SUCCESS!");
+
+            myConn.close();
+        }catch (Exception e) {
+            e.printStackTrace();
+            throw new ServletException(e);
+        }
+    }
+}
